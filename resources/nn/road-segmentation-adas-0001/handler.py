66c7923f9991de6568f0cbb95acb4139da3a5adb
Winston Fan <franva008@gmail.com>
2021-08-16 14:04:40 +1000

    refactored color map to be a separated class for re-use

--- resources/nn/road-segmentation-adas-0001/handler.py
+++ resources/nn/road-segmentation-adas-0001/handler.py
@@ -1,3 +1,6 @@
+from typing import overload
+from depthai_helpers.managers import Previews
+from resources.nn.colormap import ColorMap
 import cv2
 import numpy as np
 
@@ -5,20 +8,50 @@ from depthai_helpers.utils import to_tensor_result
 
 
 def decode(nn_manager, packet):
-    [print(f"Layer name: {l.name}, Type: {l.dataType}, Dimensions: {l.dims}") for l in packet.getAllLayers()]
-    data = np.squeeze(to_tensor_result(packet)["Output/Transpose"])
-    class_colors = [[0,0,0],  [0,255,0]]
+    # [print(f"Layer name: {l.name}, Type: {l.dataType}, Dimensions: {l.dims}") for l in packet.getAllLayers()]
+    # after squeeze the data.shape is 4,512, 896
+    data = np.squeeze(to_tensor_result(packet)["L0317_ReWeight_SoftMax"])
+    class_colors = ColorMap.COLORS
     class_colors = np.asarray(class_colors, dtype=np.uint8)
 
-    output_colors = np.take(class_colors, data, axis=0)
-    # print('decode() is called')
+    indices = np.argmax(data, axis=0)
+    indices += 8
+    output_colors = np.take(class_colors, indices, axis=0)
     return output_colors
+
 
 
 def draw(nn_manager, data, frames):
     if len(data) == 0:
         return
-    # print('draw() is called')
     for name, frame in frames:
-        if name == nn_manager.source:
-            cv2.addWeighted(frame, 1, data, 0.2, 0, frame)
+        if name in (nn_manager.source, 'host'):
+            resized = frame
+            if frame.shape != data.shape:
+                # resize the frame to fit data
+                # newsize = data.shape[:2][1],data.shape[:2][0]
+                # resized = cv2.resize(frame, newsize)
+
+                # resize the data to fit frame
+                newsize = frame.shape[:2][1],frame.shape[:2][0]
+                resized = cv2.resize(data, newsize)
+
+            # overlayed = (resized * 0.5 + data * 0.5).astype('uint8')
+            # overlayed = cv2.addWeighted(resized, 0.5, data, 0.5, 0)
+            overlayed = (frame * 0.5 + resized * 0.5).astype('uint8')
+
+            return overlayed
+            # return data
+
+# def draw(nn_manager, data, frames):
+#     if len(data) == 0:
+#         return
+#     for name, frame in frames:
+#         if name in (Previews.color.name, Previews.nn_input.name, 'host'):
+#             scale_factor = frame.shape[0] / nn_manager.input_size[1]
+#             resize_w = int(nn_manager.input_size[0] * scale_factor)
+#             resized = cv2.resize(data, (resize_w, frame.shape[0])).astype(data.dtype)
+#             offset_w = int(frame.shape[1] - nn_manager.input_size[0] * scale_factor) // 2
+#             tail_w = frame.shape[1] - offset_w - resize_w
+#             stacked = np.hstack((np.zeros((frame.shape[0], offset_w, 3)).astype(resized.dtype), resized, np.zeros((frame.shape[0], tail_w, 3)).astype(resized.dtype)))
+#             cv2.addWeighted(frame, 1, stacked, 0.2, 0, frame)
--- resources/nn/unet-camvid-onnx-0001/handler.py
+++ resources/nn/unet-camvid-onnx-0001/handler.py
@@ -6,55 +6,6 @@ from numpy import save
 
 from depthai_helpers.utils import to_tensor_result
 
-class ColorMap:        
-    SKY=[28,51,71]
-    BUILDING=[28,28,28]
-    POLE=[60,60,60]
-    ROAD=[50,25,50]
-    PAVEMENT=[95,14,91]
-    FENCE=[74,60,60]
-    VEHICLE=[0,0,56]
-    PEDESTRIAN=[86,8,23]
-    BIKE=[47,5,13]
-    UNLABELED = [17,18,21]
-    TREE=[40,40,61]
-    SIGNSYMBOL=[86,86,0]
-
-    COLORS = []
-    COLORS_BGR = []
-    COLOR_MAP = {}
-
-    # the sequence of colors in this arrar matters!!! as it maps to the prediction classes    
-    COLORS.append(SKY)
-    COLORS.append(BUILDING)
-    COLORS.append(POLE)
-    COLORS.append(ROAD)
-    COLORS.append(PAVEMENT)
-    COLORS.append(TREE)
-    COLORS.append(SIGNSYMBOL)
-    COLORS.append(FENCE)
-    COLORS.append(VEHICLE)
-    COLORS.append(PEDESTRIAN)
-    COLORS.append(BIKE)
-    COLORS.append(UNLABELED)
-
-    for color in COLORS:
-        np_color = np.array(color)
-        COLORS_BGR.append(np_color[[2,1,0]])
-
-    COLOR_MAP['SKY)'] = SKY
-    COLOR_MAP['UNLABELED'] = UNLABELED
-    COLOR_MAP['ROAD'] = ROAD
-    COLOR_MAP['PAVEMENT'] = PAVEMENT
-    COLOR_MAP['BUILDING'] = BUILDING
-    COLOR_MAP['TREE)'] = TREE
-    COLOR_MAP['FENCE)'] = FENCE
-    COLOR_MAP['POLE)'] = POLE
-    COLOR_MAP['SIGNSYMBOL)'] = SIGNSYMBOL
-    COLOR_MAP['VEHICLE)'] = VEHICLE
-    COLOR_MAP['PEDESTRIAN)'] = PEDESTRIAN
-    COLOR_MAP['BIKE)'] = BIKE
-
 def decode(nn_manager, packet):
 
     # [print(f"Layer name: {l.name}, Type: {l.dataType}, Dimensions: {l.dims}") for l in packet.getAllLayers()]
